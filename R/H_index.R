e3431d008b8b3dfe1de9f52baafe0aa38ab1f93e
coltonp2022 <coltonpadilla18@gmail.com>
2023-02-10 16:07:07 -0700

    Commit

--- R/H_index.R
+++ R/H_index.R
@@ -2,6 +2,8 @@
 #'
 #' @description Calculates the Hoover index to quantify parasite aggregation.
 #'
+#' @inheritParams bcaCI
+#'
 #' @export
 
 
@@ -14,24 +16,33 @@ H_index <- function(data,
 
   if(is.null(group)){
     if(!isTRUE(conf.int)){
+
       # Calculate the hoover index
       hoov <- do.call(sum, lapply(1:length(data[[column]]), function(i){
         return(abs(data[[column]][i] - mean(data[[column]])))
       })) / (2 * length(data[[column]]) * mean(data[[column]]))
     } else{
+
       # Calculate the hoover index using the bca bootstrap
-      hoov <- data %>%
-        pull(column) %>%
-        boot::boot(.,
-                   statistic = function(x, i){
+      dat <- data %>% # Start with the data
+        pull(column) %>% # Pull the column into a vector
+        boot::boot(., # Data from pipe
+                   statistic = function(x, i){ # Hoover index function
                      do.call(sum, lapply(1:length(x[i]), function(j){
                        return(abs(x[i][j] - mean(x[i])))
                      })) / (2 * length(x[i]) * mean(x[i]))
                    },
-                   R = r) %>%
-        boot::boot.ci(.,
-                      conf = conf,
-                      type = "bca")
+                   R = r) %>% # Run r replicates
+        boot::boot.ci(., # Output bootstrap
+                      conf = conf, # Confidence level
+                      type = "bca") # Use the bca bootstrap
+
+      # Now get a full data frame
+      hoov <- data.frame(
+        Mean = dat$t0, # Mean value
+        Lower = dat$bca[4], # Lower CI
+        Upper = dat$bca[5] # Upper CI
+      )
     }
   } else{
     if(!isTRUE(conf.int)){
@@ -41,38 +52,38 @@ H_index <- function(data,
 
         # Get the data frames for each unique group
         dat <- data %>%
-          filter(.data[[group]] == unique(data[[group]][i]))
+          filter(.data[[group]] == unique(data[[group]])[i])
 
         # Calculate the hoover index
         dat1 <- do.call(sum, lapply(1:length(dat[[column]]), function(j){
           return(abs(dat[[column]][j] - mean(dat[[column]])))
         })) / (2 * length(dat[[column]]) * mean(dat[[column]]))
 
-        # Now get a full data frame
+        # Now restructure
         dat2 <- data.frame(
-          Group = unique(data[[group]][i]),
-          Mean = dat1$t0,
-          Lower = dat1$bca[4],
-          Upper = dat1$bca[5]
+          Group = unique(data[[group]])[i],
+          Mean = dat1
         )
         colnames(dat2)[1] <- group
         return(dat2)
       }))
 
 
+
     } else{
+
       # Loop through multiple dfs
       hoov <- do.call(rbind, lapply(1:length(unique(data[[group]])), function(i){
 
         # Get a new data frame
         dat <- data %>%
-          filter(.data[[group]] == unique(data[[group]][i]))
+          filter(.data[[group]] == unique(data[[group]])[i])
 
         # Calculate the hoover index using the bca bootstrap
         dat1 <- dat %>%
           pull(column) %>%
           boot::boot(.,
-                     statistic = function(x, j){
+                     statistic = function(x, j){ # Hoover index function
                        do.call(sum, lapply(1:length(x[j]), function(k){
                          return(abs(x[j][k] - mean(x[j])))
                        })) / (2 * length(x[j]) * mean(x[j]))
@@ -84,7 +95,7 @@ H_index <- function(data,
 
         # Now get a full data frame
         dat2 <- data.frame(
-          Group = unique(data[[group]][i]),
+          Group = unique(data[[group]])[i],
           Mean = dat1$t0,
           Lower = dat1$bca[4],
           Upper = dat1$bca[5]
---
+++ R/inDISC.R
@@ -0,0 +1,111 @@
+#' Poulin's Index of Discrepancy
+#'
+#'
+#'
+
+inDISC <- function(data,
+                  column,
+                  group = NULL,
+                  conf.int = FALSE,
+                  conf = 0.95,
+                  r = 2000){
+
+
+  # If group is NULL
+  if(is.null(group)){
+    # If confidence interval = FALSE
+    if(!isTRUE(conf.int)){
+      # Bring in the data and restructure it
+      dat <- data %>%
+        dplyr::pull(.data[[column]])
+
+      # Now calculate the top
+      top <- do.call(sum, lapply(1:length(dat), function(i, j){
+        d <- abs(dat[i] - dat[j])
+      }))
+
+      # Now calculate the bottom
+      bottom <- (2 * length(dat) ^2) * mean(dat)
+
+      # Now calculate the Index of discrepancy
+      index <- top / bottom
+      return(index)
+    }
+
+    # If confidence interval = TRUE
+    else{
+
+      # Get the data and run the bootstrap
+      index <- data %>%
+        pull(.data[[column]]) %>%
+        boot(.,
+             statistic = function(x, i){
+               # Get the index
+               do.call(sum, lapply(1:length(x[i]), function(j, k){
+                 d <- abs(x[i][j] - x[i][k])
+               })) / (2 * (length(x[i]) ^ 2) * mean(x[i]))
+             },
+             R = r) %>%
+        boot.ci(.,
+                conf = conf,
+                type = "bca")
+    }
+  }
+  # If group is specified
+  else {
+    # If confidence interval = FALSE
+    if(!isTRUE(conf.int)){
+
+      # Create a new df
+      index <- do.call(rbind, lapply(1:length(unique(data[[group]])), function(g){
+
+        # Bring in the data and restructure it
+        dat <- data %>%
+          dplyr::filter(.data[[group]] == unique(data[[group]][g])) %>%
+          dplyr::pull(.data[[column]])
+
+        # Now calculate the top
+        top <- do.call(sum, lapply(1:length(dat), function(i, j){
+          d <- abs(dat[i] - dat[j])
+        }))
+
+        # Now calculate the bottom
+        bottom <- (2 * length(dat) ^2) * mean(dat)
+
+        # Now calculate the Index of discrepancy
+        i <- top / bottom
+        return(i)
+      }))
+    }
+
+    # If confidence interval = TRUE
+    else{
+
+      # Get values for each group
+      index <- do.call(rbind, lapply(1:length(unique(data[[group]])), function(g){
+
+        # Run the bootstrap
+        i <- rodent %>%
+          dplyr::filter(sex == unique(data[[group]])[g]) %>%
+          pull(.data[[column]]) %>%
+          boot(.,
+               statistic = function(x, i){
+                 # Get the index
+                 do.call(sum, lapply(1:length(x[i]), function(j, k){
+                   d <- abs(x[i][j] - x[i][k])
+                 })) / (2 * (length(x[i]) ^ 2) * mean(x[i]))
+               },
+               R = 2000) %>%
+          boot.ci(.,
+                  conf = 0.95,
+                  type = "bca")
+        return(i)
+      }))
+    }
+  }
+ return(index)
+}
+
+
+
+
