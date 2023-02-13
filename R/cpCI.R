898e7c85385b3bd3f6570047ba60d14d97eba793
coltonp2022 <coltonpadilla18@gmail.com>
2023-02-13 09:09:37 -0700

    Stop exporting cpCI

--- R/cpCI.R
+++ R/cpCI.R
@@ -9,7 +9,6 @@
 #' @return Returns an object in the form of a data frame that includes naive prevalence, and confidence intervals. If data is grouped using the group argument, this data frame also includes a column with your original groups.
 #'
 #'
-#' @export
 
 cpCI <- function(data, column, conf = 0.95, group = NULL){
 
