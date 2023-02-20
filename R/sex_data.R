5d960efe7138573e92a4b954bfe4210e9c86b1fe
coltonp2022 <coltonpadilla18@gmail.com>
2023-02-20 09:43:58 -0700

    Add the test dataset

---
+++ R/sex_data.R
@@ -0,0 +1,14 @@
+#' Simulated Parasite Dataset
+#'
+#' A simulated data set of parasite presence and intensity. Data was simulated from my own parasitology data.
+#'
+#' @format ## `sex`
+#'
+#' \describe{
+#'   \item{sex}{Sex of the host}
+#'   \item{intensity}{Parasite intensity values of these hosts}
+#'   \item{presence}{Values indicating presence of parasites on hosts}
+#'   ...
+#' }
+#'
+"sex"
